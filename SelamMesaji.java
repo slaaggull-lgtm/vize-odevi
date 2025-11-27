0097867f980923756c886805e4b87aad3aea61e1
slaaggull-lgtm <slaaggull@gmail.com>
2025-11-27 19:15:57 +0300

    Create SelamMesaji.java

---
+++ SelamMesaji.java
@@ -0,0 +1,8 @@
+public class SelamMesaji {
+    public static void main(String[] args) {
+        String ad = "Sıla";
+        String soyad = "Ağgül";
+        String tamAd = ad + " " + soyad;
+        System.out.println("Merhaba " + tamAd);
+    }
+}
