667d22ac08886fde8bd6f381ad55f8b7e79c421b
slaaggull-lgtm <slaaggull@gmail.com>
2025-11-27 19:17:35 +0300

    Create YasKontrolu.java

---
+++ YasKontrolu.java
@@ -0,0 +1,11 @@
+public class YasKontrolu {
+    public static void main(String[] args) {
+        int yas = 17;
+
+        if (yas >= 18) {
+            System.out.println("Reşitsin.");
+        } else {
+            System.out.println("Reşit değilsin.");
+        }
+    }
+}
