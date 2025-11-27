4bfb6870246e91ae3ca5e7598edad6bbe00b44ec
slaaggull-lgtm <slaaggull@gmail.com>
2025-11-27 19:16:55 +0300

    Create ToplamaOrnegi.java

---
+++ ToplamaOrnegi.java
@@ -0,0 +1,8 @@
+public class ToplamaOrnegi {
+    public static void main(String[] args) {
+        int sayi = 5;
+        double oran = 2.5;
+        double sonuc = sayi + oran;
+        System.out.println("Sonuç: " + sonuc);
+    }
+}
