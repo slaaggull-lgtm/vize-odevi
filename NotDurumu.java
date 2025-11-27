ec7997188a25999973b55709933d8944ae0c916a
slaaggull-lgtm <slaaggull@gmail.com>
2025-11-27 19:14:33 +0300

    Create NotDurumu.java

---
+++ NotDurumu.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class NotDurumu {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Notunu gir (0-100): ");
+        double not = input.nextDouble();
+
+        if (not >= 50) {
+            System.out.println("Geçtin.");
+        } else {
+            System.out.println("Kaldın.");
+        }
+    }
+}
